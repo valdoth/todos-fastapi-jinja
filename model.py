35b27bafb7cc3d539c89725fe832f7890cb2dcb4
valdoth <valdotsiarohasina@gmail.com>
2022-08-19 09:15:37 +0300

    api completed

---
+++ model.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {"example": {"item": "Read the next chapter of the book"}}
+
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {"item": "Example schema 1!"},
+                    {"item": "Example schema 2!"}
+                ]
+            }
+        }
